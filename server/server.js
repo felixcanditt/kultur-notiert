c5200efd640ba4dd1fcc6e396a45043efb5c0da7
Felix Canditt <felix.canditt@gmail.com>
2021-07-03 15:20:53 +0200

    implemented remote db replacing local db. it works :)

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import watchlistRoutes from './routes/watchlist.routes.js';
 import libraryRoutes from './routes/library.routes.js';
 
-const connectionString = 'mongodb://localhost:27017/kultur-notiert';
+dotenv.config();
+const server = express();
+
+const DB_NAME = process.env.DB_NAME || 'kultur-notiert';
+const connectionString =
+  process.env.DB_CONNECTION || 'mongodb://localhost:27017/' + DB_NAME;
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
 });
-
-const server = express();
 
 server.use(cors());
 server.use(express.json());
